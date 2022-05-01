696fc2757323d5d3c8205a8d202850cc6f97dc9b
cyildirim23 <87164546+cyildirim23@users.noreply.github.com>
2022-05-01 14:52:06 -0500

    Update 4750_TP_ESP32_NodeMCS.ino
    Comments

--- 4750_TP_ESP32_NodeMCS.ino
+++ 4750_TP_ESP32_NodeMCS.ino
@@ -13,22 +13,21 @@ String readChar;
 String BTreadChar;
 
 void setup() {
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+  SerialBT.begin("ESP32test");                        //Bluetooth device name
+  Serial.begin(115200);                               //Initialize serial monitor
   Serial.println("The device started, now you can pair it with bluetooth!");
-  Serial2.begin(115200, SERIAL_8N1, RX2, TX2);
+  Serial2.begin(115200, SERIAL_8N1, RX2, TX2);        //Initialize UART connection
 }
 
 void loop() {
-    if ((Serial2.available() > 1) && (SerialBT.available() == 0))
-    {
-      readChar = Serial2.readString(); 
+    if ((Serial2.available() > 1) && (SerialBT.available() == 0))     //If UART data is being received and BT
+    {                                                                 //data from phone isn't being received
+      readChar = Serial2.readString();                                //Print UART data to serial monitor, and send over BT
       SerialBT.print(readChar);
       Serial.print(readChar);
     }
-    else if (SerialBT.available() > 1) {
-      BTreadChar = SerialBT.readString();
+    else if (SerialBT.available() > 1) {                              //else if BT data is being received, print to
+      BTreadChar = SerialBT.readString();                             //serial monitor
       Serial.print(BTreadChar);
     }
     delay(1000);
